8c744500cbe80054ce89e236d9a8c647a517b287
soohyeon01 <ssuu0216@gmail>
2025-10-02 21:48:21 +0900

    error fix

--- src/test/java/springboot_developer/spring_blog/SpringBlogApplicationTests.java
+++ src/test/java/springboot_developer/spring_blog/SpringBlogApplicationTests.java
@@ -3,7 +3,7 @@ package springboot_developer.spring_blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class SpringBlogApplicationTests {
 
 	@Test
